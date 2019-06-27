aa4688fc18b8da9d75041be39783c8312f2c8491
Emna Trabelsi <etrabelsi@baylibre.com>
2019-06-27 10:13:11 +0200

    power.sh

--- power.sh
+++ power.sh
@@ -12,13 +12,17 @@ lava-sync clients
 JOBID=$(lava-group target | cut -d' ' -f1)
 LAVAURI=http://10.2.3.2:10080/RPC2
 lavacli --uri $LAVAURI jobs show $JOBID
-file1=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
+cd ../..
+cat uuid
+y=$(cut -d _ -f1 uuid)
+
+file1=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference curl_1 --result pass --reference $file1
-file2=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
+file2=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference curl_2 --result pass --reference $file2
-file3=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_8.csv" http://10.2.3.2:8000/artifacts/output_files/)
+file3=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_8.csv" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference curl_3 --result pass --reference $file3
-file4=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_8.csv" http://10.2.3.2:8000/artifacts/output_files/)
+file4=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_8.csv" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference curl_4 --result pass --reference $file4	  
 
 # TODO get device name
