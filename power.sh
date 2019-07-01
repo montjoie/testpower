d586a0af32fc89301f7c1294359699adc9461d40
Emna Trabelsi <etrabelsi@baylibre.com>
2019-07-01 16:07:02 +0200

    mesure

--- power.sh
+++ power.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 
-apt -q -y install libxml2 libxml2-dev bison flex libcdk5-dev libavahi-client-dev cmake git
-apt -q -y install lavacli
+apt -q -y install libxml2 libxml2-dev bison flex libcdk5-dev libavahi-client-dev cmake git || exit $?
+apt -q -y install lavacli || exit $?
 cd acme-utils/pyacmecapture
-apt -q -y install python python-libiio python-numpy python-colorama
-apt -q -y install iputils-ping
+apt -q -y install python python-libiio python-numpy python-colorama || exit $?
+apt -q -y install iputils-ping || exit $?
 lava-group >> jobsid
 cat jobsid
 LAVAURI=http://10.2.3.2:10080/RPC2
@@ -26,15 +26,13 @@ lava-send lava_start
 ./pyacmecapture.py --ip $probe_ip -d 60 -s $probe_channel -o boot_measurements -od .
 lava-sync clients
 ./pyacmecapture.py --ip $probe_ip -d 50 -s $probe_channel -o test_measurements -od .
-echo $JOBID 
-ls
-pwd
-file1=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
+y=$(sed -n $i'p' jobsid | awk '{print $1'})
+file1=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/ || exit $?)
 lava-test-reference file1 --result pass --reference $file1
-file2=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
+file2=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference file_2 --result pass --reference $file2
-file3=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_$probe_channel.csv" http://10.2.3.2:8000/artifacts/output_files/)
+file3=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_$probe_channel.csv" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference file3 --result pass --reference $file3
-file4=$(curl -F "path=@/lava-$JOBID/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_$probe_channel.csv" http://10.2.3.2:8000/artifacts/output_files/)
+file4=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_$probe_channel.csv" http://10.2.3.2:8000/artifacts/output_files/)
 lava-test-reference file4 --result pass --reference $file4	  
 done
