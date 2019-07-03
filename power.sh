fe93526e201f59660cd5e4cfd4c5708178ca503b
Emna Trabelsi <etrabelsi@baylibre.com>
2019-07-03 16:45:56 +0200

    mesure

--- power.sh
+++ power.sh
@@ -10,9 +10,6 @@ cd acme-utils/pyacmecapture  || exit $?
 apt -q -y install python python-libiio python-numpy python-colorama || exit $?
 apt -q -y install iputils-ping || exit $?
 lava-group >> jobsid || exit $?
-LAVAURI=http://10.2.3.2:10080/RPC2
-echo $LAVAURI >> uri
-DISPATCHER_IP=$(cut -d: -f2 uri | tr -d "//")
 devicesnb=$(wc -l jobsid | awk '{print $1}')
 echo $devicesnb
 for i in `seq 1 $devicesnb`;
@@ -20,9 +17,9 @@ do
 	JOBID=$(sed -n $i'p' jobsid | awk '{print $1'})
 	echo $JOBID	
 	devicename=""
-	lavacli --uri $LAVAURI jobs show $JOBID >> dict
+	lavacli --uri $LAVA_URI jobs show $JOBID >> dict
 	devicename=$(grep 'device ' dict | cut -d: -f2)
-	lavacli --uri $LAVAURI devices dict get $devicename >> file 
+	lavacli --uri $LAVA_URI devices dict get $devicename >> file 
 	probe_ip=$(grep 'probe_ip' file | awk '{print $6}' | tr -d "'" | tr -d ',')
 	if [ -z $probe_ip ] 
 	then
