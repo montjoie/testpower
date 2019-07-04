7deb6eabe409a26d3f5a5706b1ad84aed7108f78
Emna Trabelsi <etrabelsi@baylibre.com>
2019-07-04 12:00:40 +0200

    mesure

--- power.sh
+++ power.sh
@@ -1,17 +1,14 @@
 #!/bin/sh 
 
-ARTI=$1
-
-export
-echo "ARTI"
-echo $ARTI
-apt -q -y install libxml2 libxml2-dev bison flex libcdk5-dev libavahi-client-dev cmake git || exit $?
-apt -q -y install lavacli || exit $?
+lava-test-case install_libiio apt -q -y install libxml2 libxml2-dev bison flex libcdk5-dev libavahi-client-dev cmake git || exit $?
+lava-test-case install_lavacli apt -q -y install lavacli || exit $?
 cd acme-utils/pyacmecapture  || exit $?
-apt -q -y install python python-libiio python-numpy python-colorama || exit $?
-apt -q -y install iputils-ping || exit $?
+lava-test-case install_modules apt -q -y install python python-libiio python-numpy python-colorama || exit $?
+lava-test-case install_ping apt -q -y install iputils-ping || exit $?
 lava-group >> jobsid || exit $?
+#devices number in the group role 
 devicesnb=$(wc -l jobsid | awk '{print $1}')
+echo "devices number"
 echo $devicesnb
 for i in `seq 1 $devicesnb`;
 do
@@ -42,12 +39,12 @@ do
 	cd ../.. || exit $?
 	cat uuid
 	y=$(cut -d _ -f1 uuid)
-	file1=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" $ARTI || exit $?)
+	file1=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements-report.txt" $ARTIFACTORIAL || exit $?)
 	lava-test-reference file1 --result pass --reference $file1
-	file2=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" $ARTI || exit $?)
+	file2=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" $ARTIFACTORIAL || exit $?)
 	lava-test-reference file_2 --result pass --reference $file2
-	file3=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_$probe_channel.csv" $ARTI || exit $?)
+	file3=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/boot_measurements_Slot_$probe_channel.csv" $ARTIFACTORIAL || exit $?)
 	lava-test-reference file3 --result pass --reference $file3
-	file4=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_$probe_channel.csv" $ARTI || exit $?) 
+	file4=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_$probe_channel.csv" $ARTIFACTORIAL || exit $?) 
 	lava-test-reference file4 --result pass --reference $file4	  
 done
