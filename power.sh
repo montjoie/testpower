dbd8243ccd98bcf3ebae24a3e3ab4da8419effb3
Emna Trabelsi <etrabelsi@baylibre.com>
2019-07-30 09:48:27 +0200

    mesure

--- power.sh
+++ power.sh
@@ -35,9 +35,20 @@ do
 	cd ../.. || exit $?
 	cat uuid
 	y=$(cut -d _ -f1 uuid) #recuperate the job id of the host
-	cd acme-utils/pyacmecapture
+	echo $y
+	cd acme-utils/pyacmecapture || exit $?
         ACME_SUMMARY=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements-report.txt" $ARTI)
-        lava-test-reference ACME_SUMMARY --result pass --reference $ACME_SUMMARY
+	if [ -s $ACME_SUMMARY ]
+	then 
+        	lava-test-reference ACME_SUMMARY --result pass --reference $ACME_SUMMARY
+        else
+		echo " file empty"
+	fi
         RAW_DATA=$(curl -F "path=@/lava-$y/0/tests/0_server/acme-utils/pyacmecapture/test_measurements_Slot_8.csv" $ARTI)
-        lava-test-reference RAW_DATA --result pass --reference $RAW_DATA	  
+	if [ -s $RAW_DATA ]
+        then
+                lava-test-reference RAW_DATA --result pass --reference $RAW_DATA
+        else
+                echo " file empty"
+        fi	  
 done
